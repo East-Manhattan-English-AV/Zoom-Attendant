59c1234a3e400c01df6a564820659579ce565d8f
Jonathan Holland <jonholland@icloud.com>
2025-04-05 17:23:36 -0400

    Update to add basic functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import LoginPage from './components/LoginPage';
+import SearchPage from './components/SearchPage';
 
 function App() {
+  const [user, setUser] = useState(null); // user holds attendant info
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user ? (
+        <SearchPage user={user} />
+      ) : (
+        <LoginPage onLoginSuccess={setUser} />
+      )}
     </div>
   );
 }
