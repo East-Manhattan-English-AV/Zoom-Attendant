dea9a4ca8e08cc6d7f3bba40b857449533f33eab
Jonathan Holland <jonholland@icloud.com>
2025-04-09 21:19:49 -0400

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+    apiKey: "AIzaSyCQnsUXYWJf6aZZ_l2D7cP-C67CAwK2TvU",
+    authDomain: "zoom-attendant.firebaseapp.com",
+    projectId: "zoom-attendant",
+    storageBucket: "zoom-attendant.firebasestorage.app",
+    messagingSenderId: "94905933207",
+    appId: "1:94905933207:web:1ad348cf2ab55a8dbc78d2",
+    measurementId: "G-7Q2SBMJ6QG"
 };
 
 const app = initializeApp(firebaseConfig);
